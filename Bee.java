2e2359ddf3ef9cd966c99597b779efb1b53e5a2f
Stewart Chan <stew.chan@gmail.com>
2022-04-29 11:18:24 -0400

    added bee

--- Bee.java
+++ Bee.java
@@ -1,15 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Dolphin here.
+ * Write a description of class Bee here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class Dolphin extends Actor
+public class Bee extends Actor
 {
     /**
-     * Act - do whatever the Dolphin wants to do. This method is called whenever
+     * Act - do whatever the Bee wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
