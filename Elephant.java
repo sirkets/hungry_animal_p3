83741428b26f04815e7f0b1dd6a77678dc7d9a46
Stewart Chan <35233916+sirkets@users.noreply.github.com>
2022-05-02 10:04:33 -0400

    elephant can move with keys

--- Elephant.java
+++ Elephant.java
@@ -1,21 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class Elephant here.
- */
 public class Elephant extends Actor
 {
-    String name;
-    
-    public Elephant(String name)
-    {
-        this.name = name;
-    }
-
     public void act()
     {
         // Add your action code here.
-        move(-1);
-        
+        if(Greenfoot.isKeyDown("a")){
+            move(-2);
+        }
+        if(Greenfoot.isKeyDown("d")){
+            move(2);
+        }
     }
 }
