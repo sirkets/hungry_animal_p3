121c375503a9b60ab8965e3b0c01a93b8041a20e
Stewart Chan <stew.chan@gmail.com>
2022-05-18 09:57:52 -0400

    resized, slowed down, flipped elephant

--- Elephant.java
+++ Elephant.java
@@ -2,33 +2,53 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Elephant extends Actor
 {
-    private GreenfootImage[] images;
+    private GreenfootImage[] rightImages;
+    private GreenfootImage[] leftImages;
+    private SimpleTimer animTimer;
+    private int delay = 0;
+    private boolean isFacingRight = true;
     
     public Elephant(){
         //GreenfootImage image = new GreenfootImage("images/elephant_idle/idle0.png");
-        images = new GreenfootImage[8];
-        images[0] = new GreenfootImage("images/elephant_idle/idle0.png");
-        for(int i = 0; i < images.length; i++){
-            images[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+        rightImages = new GreenfootImage[8];
+        leftImages = new GreenfootImage[8];
+        for(int i = 0; i < rightImages.length; i++){
+            rightImages[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            rightImages[i].scale(100,100);
+            leftImages[i] = new GreenfootImage("images/elephant_idle/idle" + i + ".png");
+            leftImages[i].mirrorHorizontally();
+            leftImages[i].scale(100,100);
         }
-        setImage(images[3]);
+        setImage(rightImages[3]);
+        animTimer = new SimpleTimer();
+        animTimer.mark();
     }
     
     int curIndex = 0;
     public void animate()
     {
-        setImage(images[curIndex]);
-        curIndex++;
-        curIndex %= 8;
+        if(animTimer.millisElapsed() > 100){
+            if(isFacingRight){
+                setImage(rightImages[curIndex]);
+            } else {
+                setImage(leftImages[curIndex]);
+            }
+            curIndex++;
+            curIndex %= 8;
+            animTimer.mark();
+        }
+        
     }
     
     public void act()
     {
         // Add your action code here.
         if(Greenfoot.isKeyDown("a")){
+            isFacingRight = false;
             move(-2);
         }
         if(Greenfoot.isKeyDown("d")){
+            isFacingRight = true;
             move(2);
         }
         
